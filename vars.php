cd65c89d36e0e4ddc217bb2a51a5bd28fd7363e4
Moana007 <moana974@wanadoo.fr>
2015-03-27 15:03:06 +0100

    db

--- vars.php
+++ vars.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 	$appId = "371666859671907";
 	$appSecret = "baf4ee6ac06ec33678ee0ef130469d7e";
 	$redirectUrl = "http://localhost/App_WeFound404/index.php";
@@ -25,16 +25,19 @@
 		// 		die('Erreur : '.$e->getMessage());
 		// }
   
+   $host_bdd='ec2-54-228-195-52.eu-west-1.compute.amazonaws.com';
+   $name_bdd='d8rdvjqhdf1v8d';
+   $user_bdd='mrzsdpvgwshhui';
+   $pass_bdd='AFpCYYVZsC73FzHhvwfVcJwYBW';
+  
    try{
-      $bdd = new PDO ('pgsql:host=ec2-54-228-195-52.eu-west-1.compute.amazonaws.com;
-						dbname=d8rdvjqhdf1v8d;charset=utf8',
-						'mrzsdpvgwshhui',
-						'AFpCYYVZsC73FzHhvwfVcJwYBW'
-					 ) or die(print_r($bdd->errorInfo()));
-      $bdd->exec("SET NAMES utf8");
+      $bdd = new PDO ("pgsql:host=".$host_bdd.";dbname=".$name_bdd."", "".$user_bdd."", "".$pass_bdd."") or die(print_r($bdd->errorInfo()));
+      $bdd->exec("SET NAMES UTF8");
       $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	}
       
+      // 'pgsql:host=ec2-54-228-195-52.eu-west-1.compute.amazonaws.com;port=5432;dbname=anydb', 'anyuser', 'pw'
+
    catch(Exeption $e){
       die("Erreur!".$e->getMessage());
    }
