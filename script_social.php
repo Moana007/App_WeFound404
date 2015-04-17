1ffa8f1ef8cbe6387f48a530f17140f7a279932f
Moana007 <moana974@wanadoo.fr>
2015-04-17 12:07:13 +0200

    d2

--- script_social.php
+++ script_social.php
@@ -1,39 +1,27 @@
 <!-- FACEBOOK -->
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&appId=371666859671907&version=v2.3";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
 
 <script>
-FB.ui({
-  method: 'pagetab',
-  redirect_uri: 'http://appwefound404.herokuapp.com/index.php'
-}, function(response){});
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '371666859671907',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+      FB.ui({
+        method: 'pagetab',
+        redirect_uri: 'http://appwefound404.herokuapp.com/index.php'
+      }, function(response){});
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&appId=371666859671907&version=v2.3";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 </script>
-
-
-<!-- <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&appId=371666859671907&version=v2.3";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script> -->
-
-<!-- Commentaires FB -->
-<!-- <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script> -->
 
 
 <script>
