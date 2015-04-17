a6889f8e101d6884f53e6ff0c2f60075c5802218
Moana007 <moana974@wanadoo.fr>
2015-04-17 13:07:05 +0200

    dd

--- logout.php
+++ logout.php
@@ -4,11 +4,11 @@ session_start();
 session_unset();
 session_destroy();
 
-require_once('facebook-php-sdk-v4-4.0-dev/autoload.php');
-use Facebook\FacebookRedirectLoginHelper;
-$helper = new FacebookRedirectLoginHelper($redirectUrl);
-$helper->getLogoutUrl($session, $next);
+// require_once('facebook-php-sdk-v4-4.0-dev/autoload.php');
+// use Facebook\FacebookRedirectLoginHelper;
+// $helper = new FacebookRedirectLoginHelper($redirectUrl);
+// $helper->getLogoutUrl($session, $next);
 
-header("location:../index.php");	
+header("location:index.php");	
 
 ?>
