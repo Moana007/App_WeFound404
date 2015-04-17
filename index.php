a7136c49bfaf77fa063d8a99486879c8fb68b9ca
Moana007 <moana974@wanadoo.fr>
2015-04-17 12:29:53 +0200

    d5

--- index.php
+++ index.php
@@ -23,29 +23,6 @@
 
 </head>
 <body>
-  
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '371666859671907',
-      xfbml      : true,
-      version    : 'v2.3'
-    });
-      FB.ui({
-        method: 'pagetab',
-        redirect_uri: 'http://appwefound404.herokuapp.com/index.php'
-      }, function(response){});
-  };
-
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&appId=371666859671907&version=v2.3";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script>
-
   <?php require_once('script_social.php'); ?>
 <?php
   if (!isset($vote))
