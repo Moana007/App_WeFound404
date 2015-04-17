a0088daa62294d6194b3204afb3fbaaab063e395
Moana007 <moana974@wanadoo.fr>
2015-04-17 13:04:08 +0200

    add logout

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php 
+
+session_start();
+session_unset();
+session_destroy();
+
+require_once('facebook-php-sdk-v4-4.0-dev/autoload.php');
+use Facebook\FacebookRedirectLoginHelper;
+$helper = new FacebookRedirectLoginHelper($redirectUrl);
+$helper->getLogoutUrl($session, $next);
+
+header("location:../index.php");	
+
+?>
